6e15108c07a0df14392a8bca7fa92046a14c87fe
Smallmaple <cometwhisk@users.noreply.github.com>
2026-09-24 23:32:49 +0800

    fix: stop upstream folder rename notice

--- functions.php
+++ functions.php
@@ -1936,7 +1936,8 @@ function theme_folder_check_on_admin_init() {
     // 获取当前父主题文件夹名称及路径
     $current_theme_path = get_template_directory();
     $theme_folder_name = basename($current_theme_path);
-    $correct_theme_folder = 'Sakurairo';
+    // Koyori is an independent derivative theme; do not rename it back to upstream.
+    $correct_theme_folder = 'koyori';
     $user_locale = get_user_locale();
 
     // 仅管理员用户处理
