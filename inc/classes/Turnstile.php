3f4ab4796cb626f67a2d7e884f4597347c67bc2c
Smallmaple <cometwhisk@users.noreply.github.com>
2026-09-25 22:46:25 +0800

    fix: preserve turnstile content while fitting login form

--- inc/classes/Turnstile.php
+++ inc/classes/Turnstile.php
@@ -14,7 +14,7 @@ class Turnstile
                 turnstile.render('.cf-turnstile', {
                     sitekey: `{$site_key}`,
                     theme: `{$theme}`,
-                    size: 'flexible',
+                    size: 'normal',
                 });
             };
         </script>
